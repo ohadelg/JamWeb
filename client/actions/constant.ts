97fa682bea41fa29bfe82548ac94f9e38a749888
Ohad Elgazi <62428798+ohadelg@users.noreply.github.com>
2024-09-15 16:36:48 +0300

    Update constant.ts

--- client/actions/constant.ts
+++ client/actions/constant.ts
@@ -6,7 +6,7 @@ export const INSTRUMENTS = ["Guitar", "Bass", "Drums", "Flute", "Singer"];
 // Web Address
 export const WEB_PROTOCOL = 'https://',
 ADDRESS = 'serverjamweb.vercel.app',
-PORT = '',
+PORT = 8080,
 URL_BEGIN = WEB_PROTOCOL+ADDRESS; //+":"+PORT;
  //'flask-production-8a1a.up.railway.app',
 
