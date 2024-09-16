a29836a88c2e33d323c7118605a15eb44d8ee94f
ohadelg <ohadelg@post.bgu.ac.il>
2024-09-16 10:04:56 +0300

    railway server

--- client/actions/constant.ts
+++ client/actions/constant.ts
@@ -5,7 +5,7 @@ export const INSTRUMENTS = ["Guitar", "Bass", "Drums", "Flute", "Singer"];
 
 // Web Address
 export const WEB_PROTOCOL = 'https://',
-ADDRESS = 'serverjamweb.vercel.app',
+ADDRESS = 'server-side-flask-production.up.railway.app',
 PORT = 8080,
 URL_BEGIN = WEB_PROTOCOL+ADDRESS; //+":"+PORT;
  //'flask-production-8a1a.up.railway.app',
