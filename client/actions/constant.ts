78a8ee036ca78b773ac5f92bc8c85b8628c3dff8
Ohad Elgazi <62428798+ohadelg@users.noreply.github.com>
2024-09-16 20:53:54 +0300

    Update constant.ts

--- client/actions/constant.ts
+++ client/actions/constant.ts
@@ -5,7 +5,7 @@ export const INSTRUMENTS = ["Guitar", "Bass", "Drums", "Flute", "Singer"];
 
 // Web Address
 export const WEB_PROTOCOL = 'https://',
-ADDRESS = 'jamweb-server2-b8b17b142bb4.herokuapp.com',
+ADDRESS = 'stagin-jamweb-73b465b0f557.herokuapp.com',
 PORT = 8080,
 URL_BEGIN = WEB_PROTOCOL+ADDRESS; //+":"+PORT;
  //'flask-production-8a1a.up.railway.app',
