fab6ec60abc15aaf2d2a7a58a45441a542bfd92d
ohadelg <ohadelg@post.bgu.ac.il>
2024-09-17 01:39:15 +0300

    login no cora

--- client/components/LoginForm.tsx
+++ client/components/LoginForm.tsx
@@ -71,6 +71,7 @@ export default function CreateLoginForm() {
                 headers: {
                     'Content-Type': 'application/json',
                 },
+                mode: 'no-cors',
                 body: JSON.stringify(formData),
             });
 
